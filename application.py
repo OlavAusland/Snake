845cf490aa126b957456995cdc0e363a57516737
Olav Ausland Onstad <olavao@uia.no>
2020-09-25 06:46:19 +0200

    First working version of snake!

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
 import random
 import time
 import sys
+import keyboard
+from modules import cmd
+from threading import Thread
 
 class Grid:
     def __init__(self, columns, rows):
@@ -13,29 +16,52 @@ class Grid:
             for y in range(self.rows):
                 self.layout.append([x, y])
 
-
 class Snake:
     def __init__(self, grid):
-        self.symbol = '■'
-        self.body = [[4, 4], [4, 5], [4, 6]]
+        self.symbol = f'{cmd.Color.green}■{cmd.Color.reset}'
+        self.body = [[4, 4], [4, 5], [4, 6], [4, 7], [4, 8]]
+        self.ghost_pos = self.body[-1]
         self.grid = grid
         self.directions = ['left', 'right', 'up', 'down']
 
     def move(self, grid, direction):
+        self.ghost_pos = self.body[-1]
+        for i in range(len(self.body)-1,0,-1):
+            self.body[i] = self.body[i-1]
 
         if(direction == 'left'):
-            for index, point in enumerate(reversed(self.body)):
-                try:
-                    self.body[index] = self.body[index - 1]
-                except Exception as error:
-                    print(error)
-                    self.body[index] = [point[0] - 1, point[1]]
-
-
+            self.body[0] = [self.body[0][0], self.body[0][1] - 1]
+            if(self.body[0][1] < 0):
+                self.body[0][1] = self.grid.columns - 1
+        if(direction == 'right'):
+            self.body[0] = [self.body[0][0], self.body[0][1] + 1]
+            if(self.body[0][1] > self.grid.columns - 1):
+                self.body[0][1] = 0
+        if(direction == 'up'):
+            self.body[0] = [self.body[0][0] - 1, self.body[0][1]]
+            if(self.body[0][0] < 0):
+                self.body[0][0] = self.grid.rows - 1
+        if(direction == 'down'):
+            self.body[0] = [self.body[0][0] + 1, self.body[0][1]]
+            if(self.body[0][0] >= self.grid.rows):
+                self.body[0][0] = 0
 
 class Apple:
     def __init__(self):
-        pass
+        self.symbol = f'{cmd.Color.red}●{cmd.Color.reset}'
+        self.position = list()
+
+    def instantiate(self, grid, snake):
+        while True:
+            self.position = [
+                random.randint(1, grid.columns),
+                random.randint(1, grid.rows)
+            ]
+
+            for point in snake.body:
+                if(point == self.position):
+                    self.instantiate()
+            break
 
 class GameManager:
     def __init__(self, grid, snake, apple):
@@ -44,29 +70,72 @@ class GameManager:
         self.apple = apple
         self.score = 0
 
-    def update(self):
-        layout = str()
+        self.apple.instantiate(self.grid, self.snake)
 
-        self.snake.move(self.grid, 'left')
+    def update(self, direction):
+        cmd.Window.clear()
+        layout = str()
+        self.snake.move(self.grid, direction)
+        if(self.isDead()):
+            sys.exit()
+        #CHANGE NAME
         for index, point in enumerate(self.grid.layout):
             if(index % self.grid.columns == 0 and index != 0):
                 layout += '\n'
-            if(point == self.snake.body[0] or point == self.snake.body[1]):
-                layout += 'X '
-            else:
-                layout += '. '
+            if(point in self.snake.body):
+                layout += f'{self.snake.symbol} '
+                continue
+            if(point == self.apple.position):
+                layout += f'{self.apple.symbol} '
+                continue
+            layout += '. '
         layout += '\n'
+        self.snake_score()
         print(layout, end=None, file=sys.stdout.flush())
 
+    def snake_score(self):
+        if(self.snake.body[0] == self.apple.position):
+            self.score += 1
+            self.snake.body.append(self.snake.ghost_pos)
+            self.apple.instantiate(self.grid, self.snake)
+
+    def isDead(self):
+        for indexA, pointA in enumerate(self.snake.body):
+            for indexB, pointB in enumerate(self.snake.body):
+                if(pointA == pointB and indexA != indexB):
+                    return True
+        return False
+
+direction = 'left'
 
 def main():
-    grid = Grid(10, 10)
+    Thread(target=input).start()
+    Thread(target=gameLoop).start()
+
+def input():
+    global direction
+    while True:
+        if(keyboard.is_pressed('w')):
+            direction = 'up'
+        elif(keyboard.is_pressed('a')):
+            direction = 'left'
+        elif(keyboard.is_pressed('s')):
+            direction = 'down'
+        elif(keyboard.is_pressed('d')):
+            direction = 'right'
+        time.sleep(0.01)
+
+def gameLoop():
+    global direction
+    grid = Grid(25, 25)
     snake = Snake(grid)
     apple = Apple()
     gameManager = GameManager(grid, snake, apple)
-    for _ in range(4):
-        gameManager.update()
-        time.sleep(1)
+    while True:
+        gameManager.update(direction)
+        if(gameManager.isDead()):
+            break
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     main()
